57c59e488f6f59997a50edef64730422a80e9cac
joko <nautizaku@gmail.com>
2015-01-30 11:46:50 +0700

    Create js_ishop_idc_options.js

---
+++ js_ishop_idc_options.js
@@ -0,0 +1,21 @@
+/*
+ * IMPORTANT NOTES:
+ * The parameters below must be in the correct format, including length of them. 
+ * */
+
+
+var jsCountdownDefaultOptions = jsCountdownDefaultOptions || {};
+
+jsCountdownDefaultOptions.daysColor 	= 	"#f9b401"; /*menentukan warna outline pada hari*/
+jsCountdownDefaultOptions.hoursColor 	= 	"#23d0a2"; /*menentukan warna outline pada jam*/
+jsCountdownDefaultOptions.minutesColor 	= 	"#ff5153"; /*menentukan warna outline pada menit*/
+jsCountdownDefaultOptions.secondsColor 	= 	"#11c8ea"; /*menentukan warna outline pada detik*/
+
+/*menentukan tanggal publish*/
+jsCountdownDefaultOptions.date 		= 	"2015-11-14";
+
+/*menentukan waktu*/
+jsCountdownDefaultOptions.time 		= 	"15:00:00";
+
+/*menentukan pembagian wilayah waktu setempat*/
+jsCountdownDefaultOptions.timezone 		= 	"GMT+07:00";
